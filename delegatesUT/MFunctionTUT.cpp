a5a4b7ac5ef7e935ec99a3e59276c39bc85e980f
Vincent Pham <minhpta@outlook.com>
2019-01-13 00:07:04 +0700

    add new delegate class. prepare to replace MFunction.

--- delegatesUT/MFunctionTUT.cpp
+++ delegatesUT/MFunctionTUT.cpp
@@ -11,10 +11,12 @@
 #include "../ffscriptUT/fftest.hpp"
 #include "../ffscript/ffscript.h"
 #include "../ffscriptUT/TemplateForTest.hpp"
+#include "../ffscript/function/CachedMethod.hpp"
 #include "MethodsForTest.h"
 
 using namespace std;
 using namespace ffscript;
+using namespace FT;
 
 namespace ffscriptUT
 {
@@ -22,5 +24,24 @@ namespace ffscriptUT
 	{
 		MFunctionT<MethodContainer, void> memberFunction(this, &MethodContainer::memberFoo);
 		memberFunction.call(nullptr, nullptr);
+	}
+
+	FF_TEST_METHOD(MethodContainer, methodArgumentBufferTestCachedDelegate1)
+	{
+		CachedMethodDelegate<MethodContainer, void> memberFunction(this, &MethodContainer::memberFoo);
+	}
+
+	FF_TEST_METHOD(MethodContainer, methodArgumentBufferTestCachedDelegate2)
+	{
+		CachedMethodDelegate<MethodContainer, double, float, double> memberFunction(this, &MethodContainer::memberFoo);
+
+		float arg1 = 0;
+		double arg2 = 1;
+		memberFunction.setArgs(arg1, arg2);
+		memberFunction.call();
+
+		auto ret = memberFunction.getReturnValAsDouble();
+
+		FF_EXPECT_EQ(MethodContainer::memberFoo(arg1, arg2), ret);
 	}
 }
